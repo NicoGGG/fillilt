c8ec125a88012b776e4f8c50dd41aebc1fca600f
Douglas ENGSTRAND <dengstra@e1r7p2.42.fr>
2017-05-03 18:27:50 +0200

    douglas h file

---
+++ fillit.h
@@ -0,0 +1,19 @@
+#ifndef	FILLIT_H
+# define FILLIT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+#          include <stdio.h>
+
+typedef struct	s_tetri
+{
+	char			c;
+	int				x;
+	int				y;
+	struct s_tetri	*next;
+}				t_tetri;
+
+int		input_checker(char *input);
+char		**ft_strsplit(char const *s, char c);
+#endif
